93dab7e9d3ddde3934f3ecbd8dc92437bcb0611e
Eimantas Jurenkovas <100865215+Eimantas0609@users.noreply.github.com>
2023-04-13 20:20:01 +0300

    first commit

---
+++ Python_Project_1/smart_calc.py
@@ -0,0 +1,13 @@
+import numexpr
+from colorama import init
+from colorama import Fore
+init()
+
+print(Fore.CYAN)
+
+expr = input('Mathematical expression  ')
+result = numexpr.evaluate(expr)
+
+print(Fore.GREEN)
+
+print(f'Result: {result}')
